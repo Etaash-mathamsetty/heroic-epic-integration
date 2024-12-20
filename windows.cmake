f2a0531036becc829967c2f0f34e5ef8ee5cc0de
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-12-20 18:12:03 -0500

    fix CI building

--- windows.cmake
+++ windows.cmake
@@ -4,4 +4,4 @@ set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 set(WIN_LIBRARIES mingw32 opengl32 uuid gdi32 comdlg32 dwmapi libstdc++.a libgcc.a libgcc_eh.a libwinpthread.a)
 
-add_link_options(-municode)
+set(LINK_OPTIONS -municode)
