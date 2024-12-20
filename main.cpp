4df04caa00e98065c4ab99732c181fe0c754e289
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-12-20 17:45:54 -0500

    support executing unix paths

--- main.cpp
+++ main.cpp
@@ -4,21 +4,55 @@
 
 #include <iostream>
 
-int main(int argc, char **argv) {
+#define CP_UNIXCP     65010 /* Wine extension */
+
+WCHAR* convert_to_win32(const WCHAR *unixW) {
+    typedef WCHAR* (CDECL *wine_get_dos_file_name_PFN)(const char* path);
+
+    HMODULE k32 = LoadLibraryW(L"kernel32");
+    wine_get_dos_file_name_PFN pwine_get_dos_file_name =
+        (wine_get_dos_file_name_PFN)GetProcAddress(k32, "wine_get_dos_file_name");
+
+    if (!pwine_get_dos_file_name) return NULL;
+
+    int r = WideCharToMultiByte(CP_UNIXCP, 0, unixW, -1, NULL, 0, NULL, NULL);
+
+    if (!r) return NULL;
+
+    char *unixA = (char*)malloc(r);
+    r = WideCharToMultiByte(CP_UNIXCP, 0, unixW, -1, unixA, r, NULL, NULL);
+
+    if (!r) return NULL;
+
+    WCHAR *ret = pwine_get_dos_file_name(unixA);
+    free(unixA);
+
+    return ret;
+}
+
+int wmain(int argc, WCHAR **argv) {
     if(argc < 2) return -1;
 
-    std::string args;
-    std::string exe = argv[1];
+    std::wstring args;
+    std::wstring exe = argv[1];
 
     for(int i = 2; i < argc; i++)
     {
-        args += " ";
+        args += L" ";
         args += argv[i];
     }
 
-    std::cout << "executing: " << exe << " " << args << std::endl;
+    /* handle a unix path */
+    if (exe[0] == L'/' || (exe[0] == L'"' && exe[1] == L'/')) {
+        WCHAR* temp = convert_to_win32(exe.c_str());
+        if (!temp) return -1;
+        exe = temp;
+        HeapFree(GetProcessHeap(), 0, temp);
+    }
 
-    ShellExecuteA(NULL, NULL, exe.c_str(), args.c_str(), NULL, SW_SHOWNORMAL);
+    std::wcout << "executing: " << exe << L" " << args << std::endl;
+
+    ShellExecuteW(NULL, NULL, exe.c_str(), args.c_str(), NULL, SW_SHOWNORMAL);
 
     ShowWindow(GetConsoleWindow(), SW_HIDE);
 
@@ -27,20 +61,20 @@ int main(int argc, char **argv) {
 
     if(hProcessSnap == INVALID_HANDLE_VALUE) return -1;
 
-    PROCESSENTRY32 pe32;
-    pe32.dwSize = sizeof(PROCESSENTRY32);
+    PROCESSENTRY32W pe32;
+    pe32.dwSize = sizeof(PROCESSENTRY32W);
 
-    if(Process32First(hProcessSnap, &pe32))
+    if(Process32FirstW(hProcessSnap, &pe32))
     {
         do
         {
-            if(strstr(exe.c_str(), pe32.szExeFile))
+            if(wcsstr(exe.c_str(), pe32.szExeFile))
             {
                 process = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, pe32.th32ProcessID);
                 break;
             }
 
-        } while(Process32Next(hProcessSnap, &pe32));
+        } while(Process32NextW(hProcessSnap, &pe32));
     }
 
     if(process == INVALID_HANDLE_VALUE)
